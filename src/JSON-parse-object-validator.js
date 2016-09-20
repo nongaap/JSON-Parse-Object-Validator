e3eed139add5c2ca89540095682bb5273fb14505
Mike Puangmalai <mike@nongaap.com>
2016-09-20 16:31:02 -0700

    refactored function based on results of unit tests.

--- src/JSON-parse-object-validator.js
+++ src/JSON-parse-object-validator.js
@@ -1,12 +1,11 @@
-'use strict';
 // Validates that a string passed into JSON.parse converts to object
 // Returns true or false.
 // If JSON.parse causes error, function will return false and console.log error.
 
 function validatedParse(string) {
-  const parsedString = JSON.parse(string);
-
   try {
+    // JSON.parse moved inside try to prevent parse errors breaking the function
+    const parsedString = JSON.parse(string);
     // If statement handles JSON.parse cases that don't cause errors but aren't objects
     // Neither JSON.parse(false) nor JSON.parse(1234) cause errors
     if (parsedString && typeof parsedString === 'object') {
@@ -18,3 +17,5 @@ function validatedParse(string) {
 
   return false;
 }
+
+module.exports = validatedParse;
